4430d29b6bcfd9d22ca1c5ebcf8a930a3e628bad
N3pstr4un <lele.nemogarcia@gmail.com>
2026-08-10 11:59:40 -0300

    Add files via upload

---
+++ meus-scripts-nodejs/hello.js
@@ -0,0 +1,10 @@
+// Este é um comentário - o Node.js ignora esta linha
+//Comentários servem para explicar o que o código faz
+
+// Esa linha exibe uma mensagem no terminal
+console.log("Olá, mundo! Este é meu primeiro script Node.js!");
+
+//Vamos exibir mais algumas coisas interessantes 
+console.log("Parabens! Você está programando!");
+console.log("Node.js versão:", process.version);
+console.log("Sistema operacional:",process.platform);
